12c9f22210eda9dadeac70bf17d8ef34f41511c1
milewski <rafael.milewski@gmail.com>
2024-11-30 14:39:25 +0800

    more cleanups

--- src/InlineMorphTo.php
+++ src/InlineMorphTo.php
@@ -51,6 +51,10 @@ class InlineMorphTo extends MorphTo
 
         $model::saving(function (Model $parent) use ($request, $resource) {
 
+            if (blank($resource)) {
+                return $parent->{$this->attribute}()->disassociate();
+            }
+
             $prefix = sprintf('%s__', $this->attribute);
 
             $attributes = $request
@@ -79,28 +83,11 @@ class InlineMorphTo extends MorphTo
 
         });
 
-//        $morphType = $model->{$this->attribute}()->getMorphType();
-//
-//        $model->{$morphType} = !is_null($instance)
-//            ? $this->getMorphAliasForClass(get_class($instance))
-//            : null;
-//
         $foreignKey = $this->getRelationForeignKeyName($model->{$this->attribute}());
-
-//        if ($model->isDirty([ $morphType, $foreignKey ])) {
-//            $model->unsetRelation($this->attribute);
-//        }
 
         parent::fillInto($request, $model, $foreignKey);
     }
 
-    /**
-     * Resolve the field's value.
-     *
-     * @param mixed $resource
-     * @param string|null $attribute
-     * @return void
-     */
     public function resolve($resource, $attribute = null): void
     {
         $request = resolve(NovaRequest::class);
@@ -118,10 +105,8 @@ class InlineMorphTo extends MorphTo
             }
 
             /**
-             * @var ResourceDetailRequest $request
+             * @var NovaRequest $request
              */
-
-//            $request = resolve(ResourceDetailRequest::class);
             $request = $request->duplicate();
             $resolver = $request->getRouteResolver();
 
@@ -153,37 +138,12 @@ class InlineMorphTo extends MorphTo
 
             }
 
-            [ $this->morphToId, $this->morphToType ] = [
-                optional($value)->getKey(),
-                $this->resolveMorphType($resource),
-            ];
+            $this->morphToId = optional($value)->getKey();
+            $this->morphToType = $this->resolveMorphType($resource);
 
-//        if ($resourceClass = $this->resolveResourceClass($value)) {
-//            $this->resourceName = $resourceClass::uriKey();
-//        }
-//
-//        if ($value) {
-//            if (!is_string($this->resourceClass)) {
-//                $this->morphToType = $value->getMorphClass();
-//                $this->value = (string) $value->getKey();
-//
-//                if ($this->value != $value->getKey()) {
-//                    $this->morphToId = (string) $this->morphToId;
-//                }
-//
-//                $this->viewable = false;
-//            } else {
-//                $this->morphToResource = new $this->resourceClass($value);
-//
-//                $this->morphToId = Util::safeInt($this->morphToId);
-//
-//                $this->value = $this->formatDisplayValue(
-//                    $value, Nova::resourceForModel($value),
-//                );
-//
-//                $this->viewable = ($this->viewable ?? true) && $this->morphToResource->authorizedToView(app(NovaRequest::class));
-//            }
-//        }
+            if ($resourceClass = $this->resolveResourceClass($value)) {
+                $this->resourceName = $resourceClass::uriKey();
+            }
 
         } else {
 
@@ -191,292 +151,4 @@ class InlineMorphTo extends MorphTo
 
         }
     }
-
-//    /**
-//     * Create a new field.
-//     *
-//     * @param string $name
-//     * @param string|callable|null $attribute
-//     * @param callable|null $resolveCallback
-//     *
-//     * @return void
-//     */
-//    public function __construct($name, $attribute = null, callable $resolveCallback = null)
-//    {
-//
-//        parent::__construct($name, $attribute, $resolveCallback);
-//
-//        $this->meta = [
-//            'resources' => [],
-//            'listable' => true,
-//        ];
-//
-//    }
-//
-//    /**
-//     * Format:
-//     *
-//     * [ SomeNovaResource1::class, SomeNovaResource2::class ]
-//     * [ 'Some Display Text 1' => SomeNovaResource2::class, 'Some Display Text 2' => SomeNovaResource2::class ]
-//     *
-//     * @param array $types
-//     *
-//     * @return $this
-//     */
-//    public function types(array $types): self
-//    {
-//
-//        $types = collect($types)->map(function (string $resource, $key) {
-//
-//            /**
-//             * @var Resource $resourceInstance
-//             */
-//            $resourceInstance = new $resource($resource::newModel());
-//
-//            return [
-//                'className' => $resource,
-//                'uriKey' => $resource::uriKey(),
-//                'label' => is_numeric($key) ? $resource::label() : $key,
-//                'fields' => $this->resolveFields($resourceInstance),
-//            ];
-//
-//        });
-//
-//        $this->withMeta([ 'resources' => $types->values() ]);
-//
-//        return $this;
-//
-//    }
-//
-//    private function resolveFields(Resource $resourceInstance): Collection
-//    {
-//
-//        /**
-//         * @var NovaRequest $request
-//         */
-//        $request = app(NovaRequest::class);
-//        $controller = $request->route()->controller;
-//
-//        switch (get_class($controller)) {
-//
-//            case CreationFieldController::class :
-//                return $resourceInstance->creationFields($request);
-//
-//            case UpdateFieldController::class :
-//                return $resourceInstance->updateFields($request);
-//
-//            case ResourceShowController::class :
-//                return $resourceInstance->detailFields($request);
-//
-//            case ResourceIndexController::class :
-//                return $resourceInstance->indexFields($request);
-//
-//        }
-//
-//        return $resourceInstance->availableFields($request);
-//
-//    }
-//
-//    /**
-//     * Resolve the field's value for display.
-//     *
-//     * @param mixed $resource
-//     * @param string|null $attribute
-//     * @return void
-//     */
-//    public function resolveForDisplay($resource, $attribute = null)
-//    {
-//
-//        /**
-//         * @var null|Model $relationInstance
-//         * @var Field $field
-//         */
-//        $attribute = $attribute ?? $this->attribute;
-//
-//        parent::resolveForDisplay($resource, $attribute);
-//
-//        if ($relationInstance = $resource->$attribute) {
-//
-//            foreach ($this->getFields($relationInstance) as $field) {
-//
-//                $field->resolveForDisplay($relationInstance);
-//
-//            }
-//
-//        }
-//
-//    }
-//
-//    /**
-//     * Resolve the field's value.
-//     *
-//     * @param mixed $resource
-//     * @param string|null $attribute
-//     * @return void
-//     */
-//    public function resolve($resource, $attribute = null)
-//    {
-//
-//        /**
-//         * @var null|Model $relationInstance
-//         * @var Field $field
-//         */
-//        $attribute = $attribute ?? $this->attribute;
-//
-//        parent::resolve($resource, $attribute);
-//
-//        if ($relationInstance = $resource->$attribute) {
-//
-//            foreach ($this->getFields($relationInstance) as $field) {
-//
-//                if ($field->computed()) {
-//
-//                    $field->computedCallback = $field->computedCallback->bindTo(
-//                        Nova::newResourceFromModel($relationInstance),
-//                    );
-//
-//                }
-//
-//                $field->resolve($relationInstance);
-//
-//            }
-//
-//        }
-//
-//    }
-//
-//    /**
-//     * Resolve the given attribute from the given resource.
-//     *
-//     * @param mixed $resource
-//     * @param string $attribute
-//     *
-//     * @return mixed
-//     */
-//    protected function resolveAttribute($resource, $attribute)
-//    {
-//        /**
-//         * @var null|Model $relationInstance
-//         * @var Field $field
-//         */
-//
-//        if ($relationInstance = $resource->$attribute) {
-//
-//            $resource = Nova::resourceForModel($relationInstance);
-//
-//            foreach ($this->getFields($relationInstance) as $field) {
-//
-//                if ($field instanceof HasOne ||
-//                    $field instanceof HasMany ||
-//                    $field instanceof BelongsToMany) {
-//
-//                    $field->meta[ 'inlineMorphTo' ] = [
-//                        'viaResourceId' => $relationInstance->id,
-//                        'viaResource' => $resource::uriKey(),
-//                    ];
-//
-//                }
-//
-//            }
-//
-//            return $resource;
-//
-//        }
-//
-//    }
-//
-//    public function fill(NovaRequest $request, $model)
-//    {
-//
-//        /**
-//         * @var Model $relatedInstance
-//         * @var Model $model
-//         * @var Resource $resource
-//         * @var Field $field
-//         */
-//
-//        $resourceClass = $request->input($this->attribute);
-//        $relatedInstance = $model->{$this->attribute} ?? $resourceClass::newModel();
-//        $resource = new $resourceClass($relatedInstance);
-//
-//        if ($relatedInstance->exists) {
-//
-//            $resource->validateForUpdate($request);
-//
-//        } else {
-//
-//            $resource->validateForCreation($request);
-//
-//        }
-//
-//        $fields = $this->getFields($relatedInstance);
-//        $callbacks = [];
-//
-//        foreach ($fields as $field) {
-//
-//            $callbacks[] = $field->fill($request, $relatedInstance);
-//
-//        }
-//
-//        $relatedInstance->saveOrFail();
-//
-//        $model->{$this->attribute}()->associate($relatedInstance);
-//
-//        return function () use ($callbacks) {
-//
-//            foreach ($callbacks as $callback) {
-//
-//                if (is_callable($callback)) {
-//
-//                    call_user_func($callback);
-//
-//                }
-//
-//            }
-//
-//        };
-//
-//    }
-//
-//    private function getFields(Model $model): Collection
-//    {
-//        $resourceClass = Nova::resourceForModel($model);
-//
-//        return $this->meta[ 'resources' ]->where('className', $resourceClass)->first()[ 'fields' ];
-//    }
-//
-//    public function jsonSerialize(): array
-//    {
-//
-//        /**
-//         * @var NovaRequest $request
-//         */
-//        $request = app(NovaRequest::class);
-//        $originalResource = $request->route()->resource;
-//
-//        /**
-//         * Temporarily remap the route resource key so every sub field thinks its being resolved by its original parent
-//         */
-//        foreach ($this->meta[ 'resources' ] as $resource) {
-//
-//            $resource[ 'fields' ] = $resource[ 'fields' ]->transform(function ($field) use ($request, $resource) {
-//
-//                $request->route()->setParameter('resource', $resource[ 'uriKey' ]);
-//
-//                if (is_array($field)) {
-//                    return $field;
-//                }
-//
-//                return $field->jsonSerialize();
-//
-//            });
-//
-//        }
-//
-//        $request->route()->setParameter('resource', $originalResource);
-//
-//        return parent::jsonSerialize();
-//
-//    }
-
 }
